39b6109bd22a90ea635addaa034b72e49a3297b5
iCLOTS <97691871+iCLOTS@users.noreply.github.com>
2023-07-06 12:32:25 -0400

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -2,9 +2,9 @@
 
 # -- Project information
 
-project = 'Lumache'
-copyright = '2021, Graziella'
-author = 'Graziella'
+project = 'iCLOTS'
+copyright = '2023, Meredith Fay'
+author = 'Meredith Fay, PhD'
 
 release = '0.1'
 version = '0.1.0'
